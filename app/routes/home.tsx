b33c02f5cb0d535dae0121c1b820e8390ec575d3
tsedey <tsedeys19@gmail.com>
2025-03-27 23:58:48 +0300

    Add utility functions and UI components with Tailwind CSS integration

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,9 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Welcome from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Tsedey Solomon" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
